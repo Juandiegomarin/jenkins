81baca36ad5020ed843159341c98500488fe9de7
JoseAngelgomezm <joseangelgmj@hotmail.com>
2024-03-07 21:26:27 +0100

    push de jose angel

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Esta es la aplicacion react para la practica de jenkins </h1>
       <p>Esta linea sera modificada</p>
+      <p>Esta linea la ha escrito jose angel</p>
     </div>
   );
 }
