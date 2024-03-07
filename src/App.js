ce6ce39b1ff537650a440d07a4faa8af0a381020
usuario <you@example.com>
2024-03-07 18:25:26 +0100

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Esta es la aplicacion react para la practica de jenkins </h1>
+      <p>Esta linea sera modificada</p>
+    </div>
+  );
+}
+
+export default App;
